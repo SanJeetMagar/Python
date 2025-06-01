d59ad5be33d472308d651dbfc89d66c5e8152d5a
Sanjeet <voidisstartingtocode@gmail.com>
2025-06-01 14:27:24 +0545

    Improved

--- Function/function_example.py
+++ Function/function_example.py
@@ -1,38 +1,38 @@
-# # Greeting
-# def greeting(Name):
-#     return f"Hello, {Name}"
-# nam = greeting("Sanjeet")
-# print(nam)
-# print("Hello,", nam)
-# # To calculate the area
-# def calculate_area(length, breadth):
-#     area = length * breadth
-#     print(area)
-#     return(area)
-# area = calculate_area(10 , 10)
-# print(area * 2)
-# # To change celsius to fahrenheit
-# def celsius_to_fahrenheit(celsius):
-#     fahrenheit = ((celsius * (9/5)) + 32)
-#     print(fahrenheit)
-#     return fahrenheit
-# add = celsius_to_fahrenheit(32)
-# print(add + 100)
+# Greeting
+def greeting(Name):
+    return f"Hello, {Name}"
+nam = greeting("Sanjeet")
+print(nam)
+print("Hello,", nam)
+# To calculate the area
+def calculate_area(length, breadth):
+    area = length * breadth
+    print(area)
+    return(area)
+area = calculate_area(10 , 10)
+print(area * 2)
+# To change celsius to fahrenheit
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = ((celsius * (9/5)) + 32)
+    print(fahrenheit)
+    return fahrenheit
+add = celsius_to_fahrenheit(32)
+print(add + 100)
 
-# # Debugging Practice
-# def greet(name):
-#     return f"{name}"
-# info = greet("Sanjeet")
-# print("You are the best" ,info , "Best luck for your future:>")
+# Debugging Practice
+def greet(name):
+    return f"{name}"
+info = greet("Sanjeet")
+print("You are the best" ,info , "Best luck for your future:>")
 
-# # Advance reuse of function
-# def apply_discount(price, discount_percent):
-#     print("Orginal price: ", price)
-#     after_discount_price = (price *0.9)
-#     print("Price after discount: ", after_discount_price)
-#     return after_discount_price
-# further_discount = apply_discount(100, 20)
-# print("Further 10% discount:", further_discount*0.9)
+# Advance reuse of function
+def apply_discount(price, discount_percent):
+    print("Orginal price: ", price)
+    after_discount_price = (price *0.9)
+    print("Price after discount: ", after_discount_price)
+    return after_discount_price
+further_discount = apply_discount(100, 20)
+print("Further 10% discount:", further_discount*0.9)
 
 import math
 def circle_stats(radius):
@@ -45,26 +45,26 @@ print("Area:", f"{stats:.2f}")           # Should print ~28.27
 
 
 
-# def sum(x,y):
-#     z = x + y
-#     return z
-# print(sum(12,12))
-# a = sum(12, 21)
+def sum(x,y):
+    z = x + y
+    return z
+print(sum(12,12))
+a = sum(12, 21)
 
-# def add_numbers(a, b):
-#     result = a + b
-#     return result  # Returns the value to the caller
+def add_numbers(a, b):
+    result = a + b
+    return result  # Returns the value to the caller
 
-# sum_result = add_numbers(5, 3)  # sum_result gets the value 8
-# print(sum_result)  # Output: 8
+sum_result = add_numbers(5, 3)  # sum_result gets the value 8
+print(sum_result)  # Output: 8
 
 
-# import math
-# def circle_stats(radius):
-#     circumference = 2 * math.pi * radius
-#     area = math.pi * (radius ** 2)
-#     return circumference , area
-# Circumference, area  = circle_stats(10)
-# print("Circumference: ", Circumference)
-# print("Circumference:" , f"{Circumference:.2f}")
-# print(f"area: {area:.3f}")
+import math
+def circle_stats(radius):
+    circumference = 2 * math.pi * radius
+    area = math.pi * (radius ** 2)
+    return circumference , area
+Circumference, area  = circle_stats(10)
+print("Circumference: ", Circumference)
+print("Circumference:" , f"{Circumference:.2f}")
+print(f"area: {area:.3f}")
--- List/list_Method.py
+++ List/list_Method.py
@@ -1,27 +1,27 @@
 marks = [12, 11 ,12,  10, 14, 16 ,19]
 name = ["Sanjeet", "Void", "Magar"]
-# print(marks)
-# pop_item = marks.pop(1)
-# print(pop_item)
-# marks.append(88) #Use when we want to add the value at last
-# marks.pop(1) #Pop the last element or  provide index then we can pop that also. Show the value which we pop
-# print(marks.extend(name)) #Its modified marks by adding name list we can also use + sign to add or also use unpacked method to get same result
-# print(marks)
+print(marks)
+pop_item = marks.pop(1)
+print(pop_item)
+marks.append(88) #Use when we want to add the value at last
+marks.pop(1) #Pop the last element or  provide index then we can pop that also. Show the value which we pop
+print(marks.extend(name)) #Its modified marks by adding name list we can also use + sign to add or also use unpacked method to get same result
+print(marks)
 
-# marks.insert(0, "Sanjeet") #First mention the index where you want to add and at last mention what you want to add
-# marks.remove(12) #remove use when we know exact we want to remove it remove the first appereance of the value
-# print(marks)
-# marks.sort() #we cant sort when string and int are there in single list
-# print(marks)
-# marks.reverse()
-# print(marks)
-# marks.clear()
-# print(marks)
-# count = marks.count(12) #Count the apperance of given number
-# print(count)
+marks.insert(0, "Sanjeet") #First mention the index where you want to add and at last mention what you want to add
+marks.remove(12) #remove use when we know exact we want to remove it remove the first appereance of the value
+print(marks)
+marks.sort() #we cant sort when string and int are there in single list
+print(marks)
+marks.reverse()
+print(marks)
+print(marks)
+count = marks.count(12) #Count the apperance of given number
+print(count)
 indexCount = marks.index(19)
 print(indexCount)
-# print(marks)
-# print(len(marks))
+print(marks)
+print(len(marks))
+marks.clear()
 
-# # print(pop_item)
+# print(pop_item)
