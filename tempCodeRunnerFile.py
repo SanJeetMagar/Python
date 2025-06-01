7fd7aed6740fb7e5dcd49c4ee8e6322795632d09
Sanjeet <voidisstartingtocode@gmail.com>
2025-06-01 16:04:59 +0545

    Login simulation

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+username = input("Enter user name: ")
+password = input("Enter password: ")
+if username == "Admin01" and password == "1234567":
+    print("Welcome You login")
+else:
+    print("Incorrext password or username")    
