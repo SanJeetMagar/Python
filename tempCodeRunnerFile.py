a68ef925d55089007c80eb2172dcf7e0f8f66ee3
Sanjeet <voidisstartingtocode@gmail.com>
2025-06-02 15:14:04 +0545

    Password Checker

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-
-        if not length:
-            feedback += "\n- at least 12 character" 
-        return feedback
-def main():
-    password = input("Create password: ")
-    result = check_password_strength(password) 
-    print("\n" + result)
