8dd189aea50ca9ad605652f42c664dbdfba2af27
Sanjeet <voidisstartingtocode@gmail.com>
2025-06-03 15:50:23 +0545

    ADDED

--- Loop/random_numGuess.py
+++ Loop/random_numGuess.py
@@ -1,19 +1,14 @@
-# #using loop 
-# import random #First we have to import random to have a guess
-# while True:
-#     num1 = random.randint(1, 10)
-#     number = int(input("Guess a number: "))
-#     print(f"The correct num is {num1}")
-#     if num1 == number:
-#         print(f"You enter num is {number}")
-#         print("Correct")
-#         break
-#     else:
-#         print("Try again")
-    
-for i in range(10):
-    if i == 3:
-        pass  # to be implemented later
+#using loop 
+import random #First we have to import random to have a guess
+while True:
+    num1 = random.randint(1, 10)
+    number = int(input("Guess a number: "))
+    print(f"The correct num is {num1}")
+    if num1 == number:
+        print(f"You enter num is {number}")
+        print("Correct")
+        break
     else:
-        print(i)
+        print("Try again")
+    
         
