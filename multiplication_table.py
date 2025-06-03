88c35180397480d28a2791b4b0fb727cd780f27d
Sanjeet <voidisstartingtocode@gmail.com>
2025-06-03 13:00:27 +0545

    Multiplication

---
+++ multiplication_table.py
@@ -0,0 +1,9 @@
+# table = int(input("Enter the number which table u want? "))
+# for i in range(1,11):
+#     result = table * i
+#         # value  = table
+#     print(f"{table} X {i} = {result}")
+
+table = int(input("Enter the table whose u want?"))
+for i in range(1, 11):
+    print(f"{table} * {i} = {table *i}")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+table = float(input("Enter the number which table u want? "))
+while True:
+    for i in range(1,11):
+        result = table * i
+        # value  = table
+        print(f"{table} X {i} = {result}")
